17fd31ca50397b39dbb2a6e490e46111abda06bf
Alexey <98alexey@mail.ru>
2024-09-05 18:55:35 +0300

    commit

--- src/components/Entrance/Entrance.jsx
+++ src/components/Entrance/Entrance.jsx
@@ -8,7 +8,7 @@ export default function Entrance({ entranceState }) {
 				<div className="entrance__wrapper">
 					<div className="entrance__title h2" dangerouslySetInnerHTML={{ __html: entranceState.title }}></div>
 					<div className="entrance__video">
-						<video src={entranceState.videoPath} autoPlay muted playsInline loop></video>
+						{/* <video src={entranceState.videoPath} autoPlay muted playsInline loop></video> */}
 					</div>
 				</div>
 				<a href='/' className="entrance__btn btn fw-500 text-20">Подключить сообщество</a>
