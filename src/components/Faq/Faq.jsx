7ef5f0cd5347ed4a47a752399c48ef94c4b335d0
Alexey <98alexey@mail.ru>
2025-04-03 19:12:40 +0300

    commit

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -26,12 +26,12 @@ export default function Faq({ block_state }) {
 					<div className="faq__accordion accordion " >
 						{block_state.faqList.map((item, i) => {
 							return (
-								<div className="accordion__item" key={i} onClick={toggleItem} itemScope="" itemType="https://schema.org/Question">
+								<div className="accordion__item" key={i} onClick={toggleItem} itemScope itemType="https://schema.org/Question">
 									<div className="accordion__btn text-32 fw-500" itemProp="name">
 										<div className="accordion__caret"></div>
 										{item.question}
 									</div>
-									<div className="accordion__body content text-21" itemScope="" itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
+									<div className="accordion__body content text-21" itemScope itemProp="acceptedAnswer" itemType="https://schema.org/Answer">
 										<div dangerouslySetInnerHTML={{ __html: item.answer }} itemProp="text"></div>
 									</div>
 								</div>
