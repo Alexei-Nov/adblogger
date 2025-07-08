a35379812bcbeefa090dd7a69e761eaaa892da28
Alexey <98alexey@mail.ru>
2025-07-08 13:44:08 +0300

    commit

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -8,20 +8,38 @@ export default function Question() {
 		<section className='section question'>
 			<div className="container">
 				<div className="question__wrapper">
-					<div className="question__title h2">Вы автор или рекламодатель?</div>
+					<div className="question__title h2">выберите вашу роль на&nbsp;платформе</div>
 					<div className="question__btns">
-						<NavLink
-							to='for-authors'
-							className="question__btn btn btn_border"
-							onClick={() => handleTracking('create-content')}
-							end
-						>Создаю контент</NavLink>
-						<NavLink
-							to='for-advertisers'
-							className="question__btn btn btn_border"
-							onClick={() => handleTracking('order-adv')}
-							end
-						>Размещаю рекламу</NavLink>
+						<div className="question__card">
+							<div className="question__card-title">Автор</div>
+							<div className="question__card-img">
+								<img src="./img/question/img-1.png" alt="img" />
+							</div>
+							<div className="question__card-desc">Буду зарабатывать на&nbsp;рекламных постах и&nbsp;клипах</div>
+							<NavLink
+								to='for-authors'
+								className="question__card-btn btn btn_border btn_rounded btn_wide text-20 fw-500"
+								onClick={() => handleTracking('create-content')}
+								end
+							>
+								Перейти
+							</NavLink>
+						</div>
+						<div className="question__card">
+							<div className="question__card-title">Рекламодатель</div>
+							<div className="question__card-img">
+								<img src="./img/question/img-2.png" alt="img" />
+							</div>
+							<div className="question__card-desc">Хочу покупать рекламу у&nbsp;авторов</div>
+							<NavLink
+								to='for-advertisers'
+								className="question__card-btn btn btn_border btn_rounded btn_wide text-20 fw-500"
+								onClick={() => handleTracking('order-adv')}
+								end
+							>
+								Перейти
+							</NavLink>
+						</div>
 					</div>
 				</div>
 			</div>
