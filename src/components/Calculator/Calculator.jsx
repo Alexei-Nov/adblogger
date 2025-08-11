c57b446b525d7d6d44c8fd9df6e5abe064c69231
Alexey <98alexey@mail.ru>
2025-08-11 16:46:51 +0300

    commit

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -25,6 +25,9 @@ export default function Calculator({ block_state }) {
 	}
 
 
+	const windowWidth = window.innerWidth
+	const [totalCostTooltip, setTotalCostTooltip] = useState(false)
+
 	return (
 		<>
 			<section className='section calculator'>
@@ -33,9 +36,7 @@ export default function Calculator({ block_state }) {
 						<div className="calculator__title text-40 fw-600" dangerouslySetInnerHTML={{ __html: block_state.title }}></div>
 						<div className="calculator__body">
 							<div className="calculator__form">
-								<div className="calculator__subtitle text-24">
-									Выберите тип контента, количество подписчиков в&nbsp;своём сообществе и&nbsp;количество просмотров
-								</div>
+								<div className="calculator__subtitle text-24" dangerouslySetInnerHTML={{ __html: block_state.desc }}></div>
 								<div className="calculator__item">
 									<div className="calculator__row calculator__row_mob">
 										<div className="calculator__item-title text-32 fw-500">
@@ -97,11 +98,34 @@ export default function Calculator({ block_state }) {
 								<div className="calculator__results-img">
 									<img src="./img/calculator/img.png" alt="img" />
 								</div>
-								<div className="calculator__results-bottom">
-									<div className="calculator__results-label text-24 fw-500">Ваш прогнозируемый доход</div>
+								<div className={"calculator__results-bottom" + (totalCostTooltip ? ' calculator__results-bottom_open' : '')}>
+									<div className="calculator__results-bottom-bg">
+										<picture>
+											<source media="(max-width: 440px)" srcSet="/img/calculator/mask-1_mob.svg" />
+											<img src="/img/calculator/mask-1.svg" alt="img" />
+										</picture>
+										<picture>
+											<source media="(max-width: 440px)" srcSet="/img/calculator/mask-2_mob.svg" />
+											<img src="/img/calculator/mask-2.svg" alt="img" />
+										</picture>
+									</div>
+									<div className="calculator__results-bottom-tooltip"
+										onClick={() => setTotalCostTooltip(!totalCostTooltip)}
+									>
+										<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M9.26143 11.4863C9.69828 11.4863 9.93824 11.2416 10.0305 10.8211C10.1105 10.2941 10.2951 10.0242 11.1134 9.54105C11.981 9.02023 12.4301 8.37391 12.4301 7.42639C12.4301 5.96433 11.2549 4.98543 9.50754 4.98543C8.18469 4.98543 7.20024 5.5188 6.80646 6.3471C6.6834 6.58555 6.62187 6.824 6.62187 7.09382C6.62187 7.57072 6.92336 7.87819 7.40943 7.87819C7.78475 7.87819 8.06163 7.70249 8.21545 7.30089C8.41234 6.74242 8.81843 6.44122 9.4214 6.44122C10.0982 6.44122 10.5658 6.86792 10.5658 7.48287C10.5658 8.06016 10.3259 8.37391 9.53216 8.85708C8.80612 9.29006 8.4308 9.7795 8.4308 10.5199V10.6078C8.4308 11.1223 8.73844 11.4863 9.26143 11.4863Z" fill="#0D6871" />
+											<path fillRule="evenodd" clipRule="evenodd" d="M9.29368 3.24053C5.76508 3.24053 2.90459 6.10102 2.90459 9.62961C2.90459 13.1582 5.76508 16.0187 9.29368 16.0187C12.8223 16.0187 15.6828 13.1582 15.6828 9.62961C15.6828 6.10102 12.8223 3.24053 9.29368 3.24053ZM1.16211 9.62961C1.16211 5.13867 4.80274 1.49805 9.29368 1.49805C13.7846 1.49805 17.4252 5.13867 17.4252 9.62961C17.4252 14.1206 13.7846 17.7612 9.29368 17.7612C4.80274 17.7612 1.16211 14.1206 1.16211 9.62961Z" fill="#0D6871" />
+											<path d="M10.3391 13.2307C10.3391 13.8081 9.87105 14.2762 9.29365 14.2762C8.71624 14.2762 8.24816 13.8081 8.24816 13.2307C8.24816 12.6533 8.71624 12.1852 9.29365 12.1852C9.87105 12.1852 10.3391 12.6533 10.3391 13.2307Z" fill="#0D6871" />
+										</svg>
+									</div>
+									<div className="calculator__results-tooltip-body content">
+										<p>Доход за продвижение товара с Ozon состоит из фиксированной выплаты и процента от продажи товара</p>
+										<p>Подробнее <a href="#anchor-calculator">здесь</a></p>
+									</div>
 									<div className="calculator__results-num fw-600">
 										<span>{totalIncome.toLocaleString("ru-RU")}</span>&nbsp;₽
 									</div>
+									<div className="calculator__results-label text-24 fw-500">Ваш прогнозируемый доход</div>
 								</div>
 							</div>
 						</div>
--- src/components/TabsBlock/TabsBlock.jsx
+++ src/components/TabsBlock/TabsBlock.jsx
@@ -10,6 +10,20 @@ export default function TabsBlock({ block_state }) {
 				<div className="container">
 					{/* <div className="tabs-block__title title h1" dangerouslySetInnerHTML={{ __html: block_state.title }}></div> */}
 					<div className="tabs-block__wrapper">
+						<div className="tabs-block__navigation">
+							<div className="tabs-block__navigation-btn tabs-block__prev" onClick={() => activeTab > 0 ? setActiveTab(activeTab - 1) : ''}>
+								<svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<circle cx="19" cy="19" r="18.1364" transform="matrix(-1 0 0 1 38 0)" fill="white" stroke="black" strokeWidth="1.72727" />
+									<path d="M20.4819 11.3264C20.8971 10.8853 21.5916 10.8635 22.0327 11.2786C22.4738 11.6937 22.4946 12.3882 22.0796 12.8293L16.3003 18.97L16.2729 18.9993L16.3003 19.0286L22.0796 25.1692C22.4944 25.6102 22.4735 26.3039 22.0327 26.719C21.5916 27.1342 20.8971 27.1132 20.4819 26.6721L13.9673 19.7512C13.5699 19.3289 13.57 18.6696 13.9673 18.2473L20.4819 11.3264Z" fill="black" stroke="white" strokeWidth="0.0863636" />
+								</svg>
+							</div>
+							<div className="tabs-block__navigation-btn tabs-block__next" onClick={() => activeTab < block_state.tabs.length - 1 ? setActiveTab(activeTab + 1) : ''}>
+								<svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<circle cx="19" cy="19" r="18.1364" fill="white" stroke="black" strokeWidth="1.72727" />
+									<path d="M17.8452 26.6736C17.4301 27.1147 16.7356 27.1365 16.2944 26.7214C15.8534 26.3063 15.8325 25.6118 16.2476 25.1707L22.0269 19.03L22.0542 19.0007L22.0269 18.9714L16.2476 12.8308C15.8327 12.3898 15.8537 11.6961 16.2944 11.281C16.7355 10.8658 17.4301 10.8868 17.8452 11.3279L24.3599 18.2488C24.7572 18.6711 24.7572 19.3304 24.3599 19.7527L17.8452 26.6736Z" fill="black" stroke="white" strokeWidth="0.0863636" />
+								</svg>
+							</div>
+						</div>
 						<div className="tabs-block__body">
 							{block_state.tabs.map((item, i) => {
 								return (
@@ -21,7 +35,7 @@ export default function TabsBlock({ block_state }) {
 										</div>
 										<picture className="tabs-block__body-img">
 											{item.img_mob &&
-												<source media="(max-width: 1024px)" srcSet={item.img_mob} />
+												<source media="(maxWidth: 1024px)" srcSet={item.img_mob} />
 											}
 											<img src={item.img} alt="img" />
 										</picture>
