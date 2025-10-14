415478354461de88c27eeb3e0ff5c8ddaa8dcb1f
Alexey <98alexey@mail.ru>
2025-10-14 14:54:24 +0300

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -45,57 +45,59 @@ export default function Footer({ footer }) {
 
 	return (
 		<>
-			<footer className='footer'>
-				<div className="container">
-					<div className="footer__top">
-						<div className="footer__logo">
-							<img src="/img/logo.svg" alt="img" />
-						</div>
-						<a
-							href={btnState.link}
-							className={"footer__btn btn btn_small btn_border " + (location.pathname == '/shops' ? 'btn_rounded' : '')}
-							onClick={() => {
-								handleTracking('registration_footer')
-								handleTracking('registration_all')
-							}}
-						>
-							{btnState.text}
-						</a>
-					</div>
-					<div className="footer__wrapper">
-						<div className="footer__body">
-							<div className="footer__title text-20">{footerState.title}</div>
-							<div className="footer__desc text-16" dangerouslySetInnerHTML={{ __html: footerState.desc }}></div>
-						</div>
-						<nav className="footer__nav nav">
-							<ul className='nav__list'>
-								<NavLink to='/top-cases/' className='nav__link nav__link_lvl1 text-20'>Кейсы</NavLink>
-								{/* <NavItem title={"Кейсы"} submenu={caseItems} /> */}
-								<NavItem title={"Документы для авторов"} submenu={documentItemsForAuthors} />
-								<NavItem title={"Помощь"} submenu={helpItems} />
-								<NavItem title={"Документы для рекламодателей"} submenu={documentItemsForAdvertisers} />
-							</ul>
-						</nav>
-					</div>
-
-					<div className="footer__bottom text-16">
-						<div className="footer__bottom-row">
-							<div className="footer__bottom-links ">
-								<span>© VK, 2025.  Все права защищены.</span>
-								<a href='https://adblogger.vk.com/documents/privacy' target='_blank' rel="noreferrer">Политика конфиденциальности</a>
-								<span>125167, г. Москва, Ленинградский проспект, д. 39, стр. 79</span>
+			{location.pathname != '/shops-chart' &&
+				<footer className='footer'>
+					<div className="container">
+						<div className="footer__top">
+							<div className="footer__logo">
+								<img src="/img/logo.svg" alt="img" />
 							</div>
-
-							<a href='https://vk.cc/cAHxhA' className="footer__bottom-item" target='_blank' rel="noreferrer">
-								Наше сообщество ВКонтакте
-								<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-									<path fillRule="evenodd" clipRule="evenodd" d="M1.54648 1.54648C0 3.09295 0 5.58197 0 10.56V11.44C0 16.418 0 18.907 1.54648 20.4535C3.09295 22 5.58197 22 10.56 22H11.44C16.418 22 18.907 22 20.4535 20.4535C22 18.907 22 16.418 22 11.44V10.56C22 5.58197 22 3.09295 20.4535 1.54648C18.907 0 16.418 0 11.44 0H10.56C5.58197 0 3.09295 0 1.54648 1.54648ZM3.71245 6.69082C3.83161 12.4108 6.69159 15.8483 11.7058 15.8483H11.99V12.5758C13.8325 12.7591 15.2257 14.1066 15.7849 15.8483H18.3883C17.6733 13.245 15.794 11.8058 14.6207 11.2558C15.794 10.5775 17.444 8.92748 17.8382 6.69082H15.4731C14.9598 8.50582 13.4383 10.1558 11.99 10.3116V6.69082H9.6249V13.0341C8.15824 12.6675 6.3066 10.8891 6.2241 6.69082H3.71245Z" fill="white" />
-								</svg>
+							<a
+								href={btnState.link}
+								className={"footer__btn btn btn_small btn_border " + (location.pathname == '/shops' ? 'btn_rounded' : '')}
+								onClick={() => {
+									handleTracking('registration_footer')
+									handleTracking('registration_all')
+								}}
+							>
+								{btnState.text}
 							</a>
 						</div>
-					</div>
-				</div >
-			</footer >
+						<div className="footer__wrapper">
+							<div className="footer__body">
+								<div className="footer__title text-20">{footerState.title}</div>
+								<div className="footer__desc text-16" dangerouslySetInnerHTML={{ __html: footerState.desc }}></div>
+							</div>
+							<nav className="footer__nav nav">
+								<ul className='nav__list'>
+									<NavLink to='/top-cases/' className='nav__link nav__link_lvl1 text-20'>Кейсы</NavLink>
+									{/* <NavItem title={"Кейсы"} submenu={caseItems} /> */}
+									<NavItem title={"Документы для авторов"} submenu={documentItemsForAuthors} />
+									<NavItem title={"Помощь"} submenu={helpItems} />
+									<NavItem title={"Документы для рекламодателей"} submenu={documentItemsForAdvertisers} />
+								</ul>
+							</nav>
+						</div>
+
+						<div className="footer__bottom text-16">
+							<div className="footer__bottom-row">
+								<div className="footer__bottom-links ">
+									<span>© VK, 2025.  Все права защищены.</span>
+									<a href='https://adblogger.vk.com/documents/privacy' target='_blank' rel="noreferrer">Политика конфиденциальности</a>
+									<span>125167, г. Москва, Ленинградский проспект, д. 39, стр. 79</span>
+								</div>
+
+								<a href='https://vk.cc/cAHxhA' className="footer__bottom-item" target='_blank' rel="noreferrer">
+									Наше сообщество ВКонтакте
+									<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+										<path fillRule="evenodd" clipRule="evenodd" d="M1.54648 1.54648C0 3.09295 0 5.58197 0 10.56V11.44C0 16.418 0 18.907 1.54648 20.4535C3.09295 22 5.58197 22 10.56 22H11.44C16.418 22 18.907 22 20.4535 20.4535C22 18.907 22 16.418 22 11.44V10.56C22 5.58197 22 3.09295 20.4535 1.54648C18.907 0 16.418 0 11.44 0H10.56C5.58197 0 3.09295 0 1.54648 1.54648ZM3.71245 6.69082C3.83161 12.4108 6.69159 15.8483 11.7058 15.8483H11.99V12.5758C13.8325 12.7591 15.2257 14.1066 15.7849 15.8483H18.3883C17.6733 13.245 15.794 11.8058 14.6207 11.2558C15.794 10.5775 17.444 8.92748 17.8382 6.69082H15.4731C14.9598 8.50582 13.4383 10.1558 11.99 10.3116V6.69082H9.6249V13.0341C8.15824 12.6675 6.3066 10.8891 6.2241 6.69082H3.71245Z" fill="white" />
+									</svg>
+								</a>
+							</div>
+						</div>
+					</div >
+				</footer >
+			}
 		</>
 	)
 }
--- src/components/Iframe/Iframe.jsx
+++ src/components/Iframe/Iframe.jsx
@@ -3,36 +3,15 @@ import './iframe.css'
 
 export default function Iframe({ block_state }) {
 
-	const [iframeHeight, setIframeHeight] = useState('8000px');
-	const iframeRef = useRef(null); // Optional, for direct iframe interaction if needed
-
-	useEffect(() => {
-		const handleMessage = (event) => {
-			// Verify the origin for security if possible, e.g., event.origin === "https://your-iframe-domain.com"
-			if (event.data && event.data.type === 'iframeHeight' && event.data.height) {
-				setIframeHeight(`${event.data.height}px`);
-			}
-		};
-
-		window.addEventListener('message', handleMessage);
-
-		return () => {
-			window.removeEventListener('message', handleMessage);
-		};
-	}, []);
-
-
 	return (
 		<>
 			<section className='iframe'>
 				<iframe
 					src={block_state.src}
-					ref={iframeRef}
 					width="100%"
-					height={iframeHeight}
+					height="100%"
 					frameBorder="0"
-					scrolling="no"
-					style={{ width: '100%', border: 'none' }}
+					style={{ width: '100%', height: '100%', border: 'none', position: 'fixed', left: '0px', top: '0px' }}
 				></iframe >
 			</section>
 		</>
