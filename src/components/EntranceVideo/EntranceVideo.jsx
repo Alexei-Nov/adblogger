ac9c25c6ac4a675b06dba11095510ad673fe3a3d
Alexey <98alexey@mail.ru>
2026-07-16 12:34:21 +0300

    commit

--- src/components/EntranceVideo/EntranceVideo.jsx
+++ src/components/EntranceVideo/EntranceVideo.jsx
@@ -22,6 +22,7 @@ export default function EntranceVideo({ block_state }) {
 					<div className="entrance-video__body">
 						<div className="entrance-video__title" dangerouslySetInnerHTML={{ __html: block_state.title }}></div>
 						<div className="entrance-video__desc text-30" dangerouslySetInnerHTML={{ __html: block_state.desc }}></div>
+						<div className="entrance-video__label" dangerouslySetInnerHTML={{ __html: block_state.label }}></div>
 
 						<div className="entrance-video__media">
 							<video src={(window.innerWidth > 1024 ? block_state.media : block_state.media_mob)} autoPlay muted playsInline></video>
