bba6afb20796da4dd3ab4594d482ba99b629a181
Alexey <98alexey@mail.ru>
2026-07-23 11:46:51 +0300

    commit

--- src/components/EntranceVideo/EntranceVideo.jsx
+++ src/components/EntranceVideo/EntranceVideo.jsx
@@ -26,6 +26,20 @@ export default function EntranceVideo({ block_state }) {
 
 						<div className="entrance-video__media">
 							<video src={(window.innerWidth > 1024 ? block_state.media : block_state.media_mob)} autoPlay muted playsInline></video>
+							<div className="entrance-video__media-gradient">
+								<svg width="1611" height="1721" viewBox="0 0 1611 1721" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<g filter="url(#filter0_f_230_1272)">
+										<ellipse cx="805.325" cy="860.194" rx="422.594" ry="488.056" transform="rotate(-16.49 805.325 860.194)" fill="#00E8FF" />
+									</g>
+									<defs>
+										<filter id="filter0_f_230_1272" x="-0.000274658" y="-0.00125122" width="1610.65" height="1720.39" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+											<feFlood floodOpacity="0" result="BackgroundImageFix" />
+											<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+											<feGaussianBlur stdDeviation="188.485" result="effect1_foregroundBlur_230_1272" />
+										</filter>
+									</defs>
+								</svg>
+							</div>
 						</div>
 
 						<div className="entrance-video__bottom">
