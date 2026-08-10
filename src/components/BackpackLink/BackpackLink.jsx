8b4861dc2f7327b8e00a2da85308de3b7f505786
Alexey <98alexey@mail.ru>
2026-08-10 16:04:53 +0300

    commit

--- src/components/BackpackLink/BackpackLink.jsx
+++ src/components/BackpackLink/BackpackLink.jsx
@@ -5,9 +5,12 @@ export default function BackpackLink({ block_state }) {
 	const block = useRef()
 	const backpack = useRef()
 	const panel = useRef()
+
+
 	useEffect(() => {
 		let timeout = setTimeout(() => {
 			block.current.classList.add('backpack-link_hide')
+			block.current.classList.remove('backpack-link_static')
 		}, 2000);
 
 		if (window.innerWidth > 570) {
@@ -19,28 +22,51 @@ export default function BackpackLink({ block_state }) {
 			block.current.addEventListener('mouseleave', (e) => {
 				timeout = setTimeout(() => {
 					block.current.classList.add('backpack-link_hide')
-				}, 5000);
+				}, 2000);
 			})
 		} else {
 			backpack.current.addEventListener('click', (e) => {
 				clearTimeout(timeout)
 				block.current.classList.remove('backpack-link_hide')
-				timeout = setTimeout(() => {
-					block.current.classList.add('backpack-link_hide')
-				}, 5000);
+				// timeout = setTimeout(() => {
+				// 	block.current.classList.add('backpack-link_hide')
+				// }, 2000);
 			})
 		}
 
 	}, [])
 
 	return (
-		<div className='backpack-link' ref={block}>
+		<div className='backpack-link backpack-link_static' ref={block}>
 			<div className="container">
 				<div className="backpack-link__wrapper" >
 					<div className="backpack-link__img" ref={backpack}>
 						<img src="/img/backpack-link/img.png" alt="img" />
 					</div>
-					<a href="" className="backpack-link__panel" target='_blank' ref={panel}>
+					<a href={block_state.link} className="backpack-link__panel" target='_blank' ref={panel}>
+						<div className="backpack-link__panel-blur">
+							<svg width="0" height="0" preserveAspectRatio="none">
+								<defs>
+									<clipPath id="glassClip" clipPathUnits="userSpaceOnUse">
+										<path
+											d="M308 30C308 13.4315 294.569 0 278 0H30C13.4315 0 0 13.4315 0 30V72C0 88.5685 13.4315 102 30 102H278H330C346.016 102 359 89.0163 359 73C359 56.9837 346.016 44 330 44H314C310.686 44 308 41.3137 308 38V30Z"
+										/>
+									</clipPath>
+								</defs>
+							</svg>
+						</div>
+						<div className="backpack-link__panel-bg">
+							<svg width="359" height="102" viewBox="0 0 359 102" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+								<path d="M308 30C308 13.4315 294.569 0 278 0H30C13.4315 0 0 13.4315 0 30V72C0 88.5685 13.4315 102 30 102H278H330C346.016 102 359 89.0163 359 73C359 56.9837 346.016 44 330 44H314C310.686 44 308 41.3137 308 38V30Z" fill="black" fillOpacity="0.54" />
+								<path d="M30 0.5H278C294.292 0.5 307.5 13.7076 307.5 30V38C307.5 41.5899 310.41 44.5 314 44.5H330C345.74 44.5 358.5 57.2599 358.5 73C358.5 88.7401 345.74 101.5 330 101.5H30C13.7076 101.5 0.5 88.2924 0.5 72V30C0.5 13.7076 13.7076 0.5 30 0.5Z" stroke="url(#paint0_linear_10603_3937)" strokeOpacity="0.7" />
+								<defs>
+									<linearGradient id="paint0_linear_10603_3937" x1="26.5" y1="-1.58533e-06" x2="359" y2="98.5" gradientUnits="userSpaceOnUse">
+										<stop stopColor="white" />
+										<stop offset="1" stopColor="#00E4E6" />
+									</linearGradient>
+								</defs>
+							</svg>
+						</div>
 						<div className="backpack-link__title" dangerouslySetInnerHTML={{ __html: block_state.title }}></div>
 						<div className="backpack-link__currency">
 							<div className="backpack-link__currency-text">₽</div>
