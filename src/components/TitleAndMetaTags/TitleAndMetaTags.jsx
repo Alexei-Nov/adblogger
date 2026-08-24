54a2c77a6daf940526ec41561468d8d1c9d4fc16
Alexey <98alexey@mail.ru>
2026-08-24 17:59:39 +0300

    commit

--- src/components/TitleAndMetaTags/TitleAndMetaTags.jsx
+++ src/components/TitleAndMetaTags/TitleAndMetaTags.jsx
@@ -4,22 +4,16 @@ const defaultTitle = "VK AdBlogger — платформа для авторов 
 const defaultDescription = "Реклама ВКонтакте в сообществах и у блогеров. Эффективная платформа для авторов контента и рекламодателей с удобным личным кабинетом. Больше 45 000 сообществ в каталоге."
 
 export default function TitleAndMetaTags({ title = defaultTitle, description = defaultDescription }) {
-    const canonicalUrl = window.location.origin + window.location.pathname
+	const canonicalUrl = window.location.origin + window.location.pathname
 
-    return (
-        <Helmet title={title}>
-            <meta property="og:title" content={title} />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content={canonicalUrl} />
-            <link rel="canonical" href={canonicalUrl} />
-            <meta
-                property="og:description"
-                content={description}
-            />
-            <meta
-                property="description"
-                content={description}
-            />
-        </Helmet>
-    )
+	return (
+		<Helmet title={title}>
+			<meta name="og:title" content={title} />
+			<meta name="og:type" content="website" />
+			<meta name="og:url" content={canonicalUrl} />
+			<link rel="canonical" href={canonicalUrl} />
+			<meta name="og:description" content={description} />
+			<meta name="description" content={description} />
+		</Helmet>
+	)
 }
